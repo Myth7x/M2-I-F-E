02307fa6d02c342aa9965efb7a0da596a22dbf3d
myth <daniel.eckstein98@web.de>
2022-09-03 05:52:46 +0200

    added attribute list to child config

--- child_config.py
+++ child_config.py
@@ -25,6 +25,7 @@ class ChildConfig(ui.Bar):
 		self.selected_child_in_project = None
 		self.ui_objects = ui_objects
 		self.selected_object = None
+		self.attributes = []
 
 		self.title = ui.TextLine()
 		self.title.SetParent(self)
@@ -41,20 +42,30 @@ class ChildConfig(ui.Bar):
 	def update(self, child_name, object_name):
 		self.element_list.ClearItem()
 
-		LogTxt("Child Config", "Updating child config for selected child: %s" % (child_name))
-	
 		self.selected_child_in_project = child_name
 		self.selected_object = object_name
 
 		attributes = []
 		for ui_class in self.ui_objects:
 			if ui_class == object_name:
+				_module = ui
+				_class = getattr(_module, ui_class)
 				for ui_class_attr in self.ui_objects[ui_class][3]:
-					_attribute = self.Attribute(ui_class_attr, None, type(self.ui_objects[ui_class][3][ui_class_attr]))
-					attributes.append(_attribute)
-
+					_attribute = getattr(_class, ui_class_attr)
+					if callable(_attribute):
+						_attribute = self.Attribute(ui_class_attr, None, type(self.ui_objects[ui_class][3][ui_class_attr]))
+						attributes.append(_attribute)
+				break
+		self.attributes = attributes
 
 		self.title.SetText("[ Child Config ] - %s <Attributes:%s>" % (self.selected_child_in_project, len(attributes)))
+
+		self.update_attributes()
+
+	# need to find a ui element that can scroll a list of ui elements
+	def update_attributes(self):
+		for attribute in self.attributes:
+			self.element_list.InsertItem(self.element_list.GetItemCount(), "%s" % (attribute))
 
 	def selected_child(self):
 		return self.element_list.GetSelectedItemText()

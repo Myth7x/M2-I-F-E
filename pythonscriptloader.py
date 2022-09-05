c0c95ce4f3f528b6c61a2800ac206e561e02918f
myth <daniel.eckstein98@web.de>
2022-09-05 23:00:14 +0200

    some fixes & we now can have infinite children in our dicts

--- pythonscriptloader.py
+++ pythonscriptloader.py
@@ -1,4 +1,5 @@
 import sys
+from listboxscroll import ListBoxScroll
 
 # CPython Modules
 import ui
@@ -15,14 +16,14 @@ class PythonScriptLoader(ui.PythonScriptLoader):
 
 	def load_children(self, parent, dicChildren):
 
-		if True == dicChildren.has_key("style"):
+		if 'style' in dicChildren:
 			for style in dicChildren["style"]:
 				parent.AddFlag(style)
 
-		if False == dicChildren.has_key("children"):
+		if 'children' not in dicChildren:
 			return False
 
-		Index = 0
+		c = 0
 
 		ChildrenList = dicChildren["children"]
 		parent.Children = range(len(ChildrenList))
@@ -37,193 +38,209 @@ class PythonScriptLoader(ui.PythonScriptLoader):
 			except KeyError:								
 				Type = ElementValue["type"] = "window"				
 
-			if FALSE == self.CheckKeyList(Name, ElementValue, self.DEFAULT_KEY_LIST):
-				del parent.Children[Index]
+			if False == self.CheckKeyList(Name, ElementValue, self.DEFAULT_KEY_LIST):
+				del parent.Children[c]
 				continue
 
 			if Type == "window":
-				parent.Children[Index] = ui.ScriptWindow()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementWindow(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ScriptWindow()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementWindow(parent.Children[c], ElementValue, parent)
 
 			elif Type == "button":
-				parent.Children[Index] = ui.Button()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementButton(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.Button()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementButton(parent.Children[c], ElementValue, parent)
 
 			elif Type == "radio_button":
-				parent.Children[Index] = ui.RadioButton()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementButton(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.RadioButton()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementButton(parent.Children[c], ElementValue, parent)
 
 			elif Type == "toggle_button":
-				parent.Children[Index] = ui.ToggleButton()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementButton(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ToggleButton()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementButton(parent.Children[c], ElementValue, parent)
 
 			elif Type == "mark":
-				parent.Children[Index] = ui.MarkBox()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementMark(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.MarkBox()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementMark(parent.Children[c], ElementValue, parent)
 
 			elif Type == "image":
-				parent.Children[Index] = ui.ImageBox()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementImage(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ImageBox()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementImage(parent.Children[c], ElementValue, parent)
 
 			elif Type == "expanded_image":
-				parent.Children[Index] = ui.ExpandedImageBox()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementExpandedImage(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ExpandedImageBox()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementExpandedImage(parent.Children[c], ElementValue, parent)
 
 			elif Type == "ani_image":
-				parent.Children[Index] = ui.AniImageBox()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementAniImage(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.AniImageBox()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementAniImage(parent.Children[c], ElementValue, parent)
 
 			elif Type == "slot":
-				parent.Children[Index] = ui.SlotWindow()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementSlot(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.SlotWindow()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementSlot(parent.Children[c], ElementValue, parent)
 
 			elif Type == "candidate_list":
-				parent.Children[Index] = ui.CandidateListBox()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementCandidateList(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.CandidateListBox()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementCandidateList(parent.Children[c], ElementValue, parent)
 
 			elif Type == "grid_table":
-				parent.Children[Index] = ui.GridSlotWindow()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementGridTable(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.GridSlotWindow()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementGridTable(parent.Children[c], ElementValue, parent)
 
 			elif Type == "text":
-				parent.Children[Index] = ui.TextLine()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementText(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.TextLine()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementText(parent.Children[c], ElementValue, parent)
 
 			elif Type == "editline":
-				parent.Children[Index] = ui.EditLine()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementEditLine(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.EditLine()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementEditLine(parent.Children[c], ElementValue, parent)
 
 			elif Type == "titlebar":
-				parent.Children[Index] = ui.TitleBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementTitleBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.TitleBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementTitleBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "horizontalbar":
-				parent.Children[Index] = ui.HorizontalBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementHorizontalBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.HorizontalBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementHorizontalBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "board":
-				parent.Children[Index] = ui.Board()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementBoard(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.Board()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementBoard(parent.Children[c], ElementValue, parent)
 
 			elif Type == "board_with_titlebar":
-				parent.Children[Index] = ui.BoardWithTitleBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementBoardWithTitleBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.BoardWithTitleBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementBoardWithTitleBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "thinboard":
-				parent.Children[Index] = ui.ThinBoard()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementThinBoard(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ThinBoard()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementThinBoard(parent.Children[c], ElementValue, parent)
 				
 			elif Type == "thinboard_gold":
-				parent.Children[Index] = ui.ThinBoardGold()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementThinBoard(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ThinBoardGold()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementThinBoard(parent.Children[c], ElementValue, parent)
 
 			elif Type == "thinboard_circle":
-				parent.Children[Index] = ui.ThinBoardCircle()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementThinBoard(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ThinBoardCircle()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementThinBoard(parent.Children[c], ElementValue, parent)
 
 			elif Type == "thinboard_gold":
-				parent.Children[Index] = ui.ThinBoardGold()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementThinBoardGold(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ThinBoardGold()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementThinBoardGold(parent.Children[c], ElementValue, parent)
 
 			elif Type == "thinboard_circle":
-				parent.Children[Index] = ui.ThinBoardCircle()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementThinBoardCircle(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ThinBoardCircle()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementThinBoardCircle(parent.Children[c], ElementValue, parent)
 
 			elif Type == "box":
-				parent.Children[Index] = ui.Box()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementBox(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.Box()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementBox(parent.Children[c], ElementValue, parent)
 
 			elif Type == "bar":
-				parent.Children[Index] = ui.Bar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.Bar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "line":
-				parent.Children[Index] = ui.Line()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementLine(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.Line()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementLine(parent.Children[c], ElementValue, parent)
 
 			elif Type == "slotbar":
-				parent.Children[Index] = ui.SlotBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementSlotBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.SlotBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementSlotBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "gauge":
-				parent.Children[Index] = ui.Gauge()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementGauge(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.Gauge()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementGauge(parent.Children[c], ElementValue, parent)
 
 			elif Type == "scrollbar":
-				parent.Children[Index] = ui.ScrollBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementScrollBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ScrollBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementScrollBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "thin_scrollbar":
-				parent.Children[Index] = ui.ThinScrollBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementScrollBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ThinScrollBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementScrollBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "small_thin_scrollbar":
-				parent.Children[Index] = ui.SmallThinScrollBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementScrollBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.SmallThinScrollBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementScrollBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "sliderbar":
-				parent.Children[Index] = ui.SliderBar()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementSliderBar(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.SliderBar()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementSliderBar(parent.Children[c], ElementValue, parent)
 
 			elif Type == "listbox":
-				parent.Children[Index] = ui.ListBox()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementListBox(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ListBox()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementListBox(parent.Children[c], ElementValue, parent)
 
 			elif Type == "listbox2":
-				parent.Children[Index] = ui.ListBox2()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementListBox2(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ListBox2()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementListBox2(parent.Children[c], ElementValue, parent)
 			elif Type == "listboxex":
-				parent.Children[Index] = ui.ListBoxEx()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementListBoxEx(parent.Children[Index], ElementValue, parent)
+				parent.Children[c] = ui.ListBoxEx()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementListBoxEx(parent.Children[c], ElementValue, parent)
 			elif Type == "listbox_scroll":
-				parent.Children[Index] = ui.ListBoxScroll()
-				parent.Children[Index].SetParent(parent)
-				self.LoadElementListBox(parent.Children[Index], ElementValue, parent)
-
+				parent.Children[c] = ListBoxScroll()
+				parent.Children[c].SetParent(parent)
+				self.LoadElementListBox(parent.Children[c], ElementValue, parent)
+			elif Type == "combo_box":
+				parent.Children[c] = ui.ComboBox()
+				parent.Children[c].SetParent(parent)
+				_ = self.load_element_combobox(parent.Children[c], ElementValue, parent)
 			else:
-				Index += 1
+				c += 1
 				continue
 
-			parent.Children[Index].SetWindowName(Name)
-			if 0 != self.InsertFunction:
-				self.InsertFunction(Name, parent.Children[Index])
+			parent.Children[c].SetWindowName(Name)
+			if self.InsertFunction:
+				self.InsertFunction(Name, parent.Children[c])
 
-			self.load_children(parent.Children[Index], ElementValue)
-			Index += 1
+			self.load_children(parent.Children[c], ElementValue)
+			c += 1
+
+	def load_element_combobox(self, window, value, parentWindow):
+
+		if False == self.CheckKeyList(value["name"], value, self.LIST_BOX_KEY_LIST):
+			return False
+
+		if value.has_key("item_align"):
+			window.SetTextCenterAlign(value["item_align"])
+
+		window.SetSize(value["width"], value["height"])
+		self.LoadDefaultData(window, value, parentWindow)
+
+		return True
 
 	def load_script(self, parent, path, script):
 		LogTxt(__name__, "Loading script: %s" % script)
@@ -271,7 +288,6 @@ class PythonScriptLoader(ui.PythonScriptLoader):
 		if "style" in _data:
 			for StyleList in _data["style"]:
 				_object.AddFlag(StyleList)
-
 
 		_object.SetPosition(int(_data["x"]), int(_data["y"]))
 		_object.SetSize(int(_data["width"]), int(_data["height"]))

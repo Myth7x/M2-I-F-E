07dc2a48ae04a22302aafcdb95e29a9b07703a2a
myth <daniel.eckstein98@web.de>
2022-09-05 22:59:02 +0200

    added scene browser

---
+++ ifmgr_ui/stylescripts/style_scene_browser.py
@@ -0,0 +1,119 @@
+import wndMgr, ui, grp
+
+from ...ifmgr_ui.board import Board_Custom
+
+SCREEN_WIDTH = wndMgr.GetScreenWidth()
+SCREEN_HEIGHT = wndMgr.GetScreenHeight()
+
+_class = Board_Custom
+
+WIDTH = 240
+HEIGHT = 430
+
+object = {
+    "name" : "n_scene_browser",
+
+    "style" : ("movable", "float",),
+
+    "sizeable" : {
+        'enabled' : False,
+        'min_width' : 200,
+        'min_height' : 400,
+        'max_width' : 500,
+        'max_height' : 900,
+    },
+
+	"x" : SCREEN_WIDTH - WIDTH - 8,
+	"y" : 70,
+
+	"width" : WIDTH,
+	"height" : HEIGHT,
+
+	"children" :
+	(
+        {
+            "name" : "board",
+            "type" : "board",
+            "style" : ("attach",), 
+
+            "x" : 0,
+            "y" : 0,
+
+            "width" : WIDTH,
+	        "height" : HEIGHT,
+
+        },
+        {
+            "name" : "titlebar_title",
+            "type" : "text",
+            "x" : 15,
+            "y" : 9,
+            "text" : "Scene Browser",
+            "text_horizontal_align" : "left",
+        },
+        {
+            "name" : "bg_object_list",
+            "type" : "thinboard",
+
+            "x" : 8,
+            "y" : 26,
+
+            "width" : WIDTH - 15,
+            "height" : 370,
+
+            "size"      : "relative",
+        },
+        {
+            "name" : "object_list",
+            "type" : "listbox_scroll",
+
+            "x" : 10 + 4,
+            "y" : 26 + 6,
+
+            "width" : WIDTH - 15 - 11,
+            "height" : 370 - 12,
+
+
+            "item_align" : wndMgr.HORIZONTAL_ALIGN_LEFT,
+
+            "size"      : "relative",
+        },
+
+        # Copy Raw Data
+        {
+        	"name" : "btn_copy_raw_data",
+        	"type" : "button",
+
+        	"x" : 8,
+        	"y" : HEIGHT - 30,
+
+        	"text" : "Copy Raw Data",
+        	"horizontal_align" : "left",
+        	"vertical_align" : "top",
+
+            "tooltip_text" : "Copy style dictionary to clipboard",
+
+        	"default_image" : "d:/ymir work/ui/public/large_button_01.sub",
+            "over_image" : "d:/ymir work/ui/public/large_button_02.sub",
+            "down_image" : "d:/ymir work/ui/public/large_button_03.sub",
+        },
+
+        # Save
+        {
+            "name" : "btn_save",
+            "type" : "button",
+
+            "x" : WIDTH - 8 - 60,
+            "y" : HEIGHT - 30,
+
+            "text" : "Save",
+
+            "tooltip_text" : "Save scene to file",
+
+            "default_image" : "d:/ymir work/ui/public/middle_button_01.sub",
+            "over_image" : "d:/ymir work/ui/public/middle_button_02.sub",
+            "down_image" : "d:/ymir work/ui/public/middle_button_03.sub",
+        },
+
+    ),
+}
---
+++ n_scene_browser.py
@@ -0,0 +1,123 @@
+# Second Window with our own scriptloader, but i realized, we gonna use the original ones for functionality
+from distutils.log import Log
+from pythonscriptloader import PythonScriptLoader
+from proto_utils import LogTxt
+
+import ui, wndMgr
+
+class n_scene_browser(ui.ScriptWindow):
+	def __init__(self):
+		ui.ScriptWindow.__init__(self)
+		LogTxt(__name__, "Initializing...")
+
+		self.style_script_data 	= None
+		self.ui_data 			= None
+		self.object 			= None
+		self.parent 			= None
+
+		self.scene = {
+			"name": "*New Scene",
+			"children": []
+		}
+
+		self.script_loader = PythonScriptLoader()
+
+		self.init = self.load()
+
+		self.ref_titlebar_title 		= self.find_object(self.object.Children, "titlebar_title")
+		self.ref_object_list 	= self.find_object(self.object.Children, "object_list")
+		self.ref_board 			= self.find_object(self.object.Children, "board")
+
+		self.ref_object_list.OnMouseWheel = self.ref_object_list.scrollBar.OnMouseWheel
+		self.ref_object_list.OnMouseLeftButtonDoubleClick = self.on_double_click_object_list
+
+		LogTxt(__name__, "Initialized!")
+
+	def set_scene_name(self, name):
+		self.scene['name'] = name
+
+	def set_parent(self, parent):
+		self.parent = parent
+
+	def on_double_click_object_list(self):
+		pass
+
+	def add_scene_object(self, data):
+		_child = {
+			'child_name': data[0],
+			'object_name': data[0],
+			'class': data[1],
+			'object': data[2]
+		}
+		LogTxt(__name__, "Adding object to scene: %s" % _child['child_name'])
+
+		self.scene['children'].append(_child)
+
+	# Finds specific object in a list of objects, used for iterating through the ui tree(children)
+	def find_object(self, objects, object_name):
+		for object in objects:
+			if object.GetWindowName() == object_name:
+				return object
+			if hasattr(object, "Children"):
+				for child in object.Children:
+					if child.GetWindowName() == object_name:
+						return child
+		return None
+
+	def set_ui_data(self, data):
+		self.ui_data = data
+		self.arrange_object_list()
+
+	def load(self):
+		try:
+			self.object = self.script_loader.load_script(self, "C:\\Proto_InterfaceManager\\ifmgr_ui\\stylescripts\\", "style_scene_browser.py")
+		except:
+			LogTxt(__name__, "Failed to load script!")
+			return False
+		return True
+
+	# return ui data dictionary
+	def get_selected_object(self):
+		if self.ref_object_list.GetItemCount() > 0:
+			if self.ref_object_list.GetSelectedItem() != -1:
+				selected_object = self.ref_object_list.GetSelectedItemText()
+				for object in self.ui_data:
+					if selected_object == object:
+						return self.ui_data[object]
+		return None
+	
+	# returns the children dictionary with set values
+	def get_selected_children(self):
+		selected_object = self.ref_object_list.GetSelectedItemText()
+		if selected_object == None:
+			return None
+
+		# Find dict in self.scene['children'] with the name of the selected object
+		for child in self.scene['children']:
+			if child['child_name'] == selected_object:
+				return child
+
+		return None
+
+	def update_title_info(self):
+		self.ref_titlebar_title.SetText("[ %s : %d children ]" % (self.scene['name'], self.ref_object_list.GetItemCount()))
+
+	def update(self):
+		if self.init == True:
+			self.update_title_info()
+
+			## do stuff here
+			if self.ref_object_list.GetItemCount() != len(self.scene['children']):
+				self.arrange_object_list()
+		
+			self.ref_object_list.OnUpdate()
+
+	def render(self):
+		pass
+
+	def arrange_object_list(self):
+		self.ref_object_list.ClearItem()
+		for child in self.scene['children']:
+			self.ref_object_list.InsertItem(self.ref_object_list.GetItemCount(), child['child_name'])
+
+

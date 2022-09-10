3b0646b2e596bd15870bd0aae5c64014593e488b
myth <daniel.eckstein98@web.de>
2022-09-10 05:08:31 +0200

    removed logs

--- n_object_browser.py
+++ n_object_browser.py
@@ -38,7 +38,6 @@ class n_object_browser(ui.ScriptWindow):
 
 	def on_double_click_object_list(self):
 		if self.ref_object_list.IsIn():
-			LogTxt(__name__, "Double clicked object list! To")
 			selected_object = self.get_selected_object()
 			if selected_object:
 				self.parent.scene_browser.add_scene_object(selected_object[0], selected_object)

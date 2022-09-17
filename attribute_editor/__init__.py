0378cb2015f0066255209a78b14093ee207c66e8
myth <daniel.eckstein98@web.de>
2022-09-18 00:32:20 +0200

    started working on attribute editor

--- attribute_editor/__init__.py
+++ attribute_editor/__init__.py
@@ -3,13 +3,11 @@ from _utils import LogTxt
 
 import ui
 
-import ifmgr_ui
-
 import globals
+from .standard_attributes import StandardAttributes, Attribute
 
 class attribute_editor(ui.ScriptWindow):
-	"""
-	attribute editor class
+	"""attribute editor class
 	- used to edit attributes of a scene object duh
 	"""
 	def __init__(self):
@@ -18,6 +16,8 @@ class attribute_editor(ui.ScriptWindow):
 
 		self.object 			= None
 		self.parent 			= None
+
+		self.attributes = []
 
 		self.scene_object = {}
 
@@ -34,38 +34,44 @@ class attribute_editor(ui.ScriptWindow):
 		self.bg_attr_editor.SetParent(self.ref_category_attribute_configuration)
 		self.bg_attr_editor.SetPosition(20, 60)
 		self.bg_attr_editor.SetSize(self.ref_category_attribute_configuration.GetWidth() - 40, self.ref_category_attribute_configuration.GetHeight() - 80)
-		self.bg_attr_editor.SetColor(0x00000000)
+		self.bg_attr_editor.SetColor(0x304a4a4a)
 		self.bg_attr_editor.Show()
 		self.attr_editbox = ui.EditLine()
 		self.attr_editbox.SetParent(self.bg_attr_editor)
-		self.attr_editbox.SetPosition(0, 0)
+		self.attr_editbox.SetPosition(3, 3)
 		self.attr_editbox.SetMultiLine()
 		self.attr_editbox.SetLimitWidth(self.bg_attr_editor.GetWidth())
-		self.attr_editbox.SetSize(*(self.bg_attr_editor.GetWidth(), self.bg_attr_editor.GetHeight()))
+		self.attr_editbox.SetSize(*(self.bg_attr_editor.GetWidth() -6, self.bg_attr_editor.GetHeight()-6))
 		self.attr_editbox.SetText("Weeeeeeeeeeeeeew\n weeew")
 		self.attr_editbox.SetMax(1000)
 		self.attr_editbox.Show()
 
 		# Create ComboBox for attribute.attribute selection
-		self.cb_on_event_val = ui.ComboBox()
-		self.cb_on_event_val.SetParent(self.ref_category_attribute_configuration)
-		self.cb_on_event_val.SetPosition(20, 25)
-		self.cb_on_event_val.SetSize(self.ref_category_attribute_configuration.GetWidth() - 40, 25)
-		self.cb_on_event_val.Show()
+		self.cb_on_attr_val = ui.ComboBox()
+		self.cb_on_attr_val.SetParent(self.ref_category_attribute_configuration)
+		self.cb_on_attr_val.SetPosition(20, 25)
+		self.cb_on_attr_val.SetSize(self.ref_category_attribute_configuration.GetWidth() - 40, 25)
+		self.cb_on_attr_val.Show()
 	
-		self.cb_on_event_val.SetCurrentItem('- Select Event -')
-		self.cb_on_event_val.SetEvent(ui.__mem_func__(self.on_select_attr_val))
-		self.cb_on_event_val.InsertItem(1, '__init__')
-		self.cb_on_event_val.InsertItem(2, 'OnUpdate')
-		self.cb_on_event_val.InsertItem(3, 'OnRender')
+		self.fn_original_on_select_attr_val = self.cb_on_attr_val.OnSelectItem
+		self.cb_on_attr_val.listBox.OnSelectItem = self.on_select_attr_val
+
+		self.attributes = []
+		for attr in StandardAttributes:
+			LogTxt(__name__, "Adding attribute %s" % attr)
+			self.attributes.append(Attribute(attr, StandardAttributes[attr]['type'], StandardAttributes[attr]['index'], 0, ""))
+		LogTxt(__name__, "Attributes: %s" % self.attributes)
+		for attr in self.attributes:
+			self.cb_on_attr_val.InsertItem(self.cb_on_attr_val.listBox.GetItemCount(), attr.name)
+		
+		self.cb_on_attr_val.SelectItem(0)
 
 		#LogTxt(__name__, "Initialized!")
 
-	def on_select_attr_val(self, selected_attr):
-		if selected_attr > 0:
-			#LogTxt(__name__, "Selected attribute: %s" % self.cb_on_event_val.listBox.textDict[selected_attr-1])
-
-			self.attr_editbox.SetText(self.scene_object["child_object"].__dict__[self.cb_on_event_val.listBox.textDict[selected_attr-1]])
+	def on_select_attr_val(self, selected_attr, name):
+		LogTxt(__name__, "Selected attribute: %s" % name)
+		self.attr_editbox.SetText(selected_attr + " " + name)
+		self.fn_original_on_select_attr_val(selected_attr, name)
 	def set_parent(self, parent):
 		self.parent = parent
 
@@ -91,10 +97,11 @@ class attribute_editor(ui.ScriptWindow):
 	def update(self):
 		if self.ref_board.IsShow():
 			self.ref_board.SetTitleName('Children Editor - %s' % self.scene_object["child_name"])
-			selected_attr = self.cb_on_event_val.listBox.GetSelectedItem()
-			if selected_attr > 0:
-				selected_attr_str = self.cb_on_event_val.listBox.textDict[selected_attr-1]
-				self.cb_on_event_val.SetCurrentItem(selected_attr_str)
+			selected_attr = self.cb_on_attr_val.listBox.GetSelectedItem()
+			selected_attr_str = self.cb_on_attr_val.listBox.textDict[selected_attr]
+			self.cb_on_attr_val.SetCurrentItem(selected_attr_str)
 
+			self.attr_editbox.SetText(self.scene_object[selected_attr_str] if selected_attr_str in self.scene_object else '')
+	
 	def render(self):
 		pass
